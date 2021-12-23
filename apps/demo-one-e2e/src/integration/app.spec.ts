d424f89502bca0001e97a38e7ef8a5debda54a54
Andrew Alderson <andrew.alderson@outlook.com>
2021-12-22 17:21:13 -0700

    add login to e2e app
    Added a cypress login command to log into ad b2c with
    ROPC and set the tokens in local storage.
    The cyrpess.env.template.json file needs to be renamed
    to cyress.env.json and the b2c config needs to be added.

--- apps/demo-one-e2e/src/integration/app.spec.ts
+++ apps/demo-one-e2e/src/integration/app.spec.ts
@@ -6,20 +6,14 @@ describe('demo-one', () => {
       delete req.headers['if-none-match']; // prevents 304 responses
     }).as('getMsalSettings');
 
-    cy.visit('/');
+    cy.login().visit('/');
   });
 
   it('should load msal setting', () => {
     cy.wait('@getMsalSettings').its('response.statusCode').should('eq', 200);
   });
 
-  // sThis test fails because user needs to be logged in
-  // skipping until we set up login command.
-  it.skip('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
+  it('should display welcome message', () => {
     getGreeting().contains('Welcome demo-one');
   });
 });
--- apps/demo-one-e2e/src/support/commands.ts
+++ apps/demo-one-e2e/src/support/commands.ts
@@ -1,25 +1,31 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { login } = require('./b2c-auth');
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
-    login(email: string, password: string): void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    login(): Cypress.Chainable;
   }
 }
-//
-// -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
+
+let cachedTokenExpiryTime = new Date().getTime();
+let cachedTokenResponse: unknown = null;
+
+Cypress.Commands.add('login', () => {
+  // Clear our cache if tokens are expired
+  if (cachedTokenExpiryTime <= new Date().getTime()) {
+    cachedTokenResponse = null;
+  }
+
+  return login(cachedTokenResponse).then((tokenResponse: unknown) => {
+    cachedTokenResponse = tokenResponse;
+    // Set expiry time to 50 minutes from now
+    cachedTokenExpiryTime = new Date().getTime() + 50 * 60 * 1000;
+  });
 });
+
 //
 // -- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
