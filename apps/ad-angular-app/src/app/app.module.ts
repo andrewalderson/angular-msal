71d45adba7197aee628c05ddc9c35b9aaf0afaf9
Andrew Alderson <andrew.alderson@outlook.com>
2021-12-29 22:09:40 -0700

    add Angular app for MSAL with Azure AD

---
+++ apps/ad-angular-app/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([{ path: '', component: NxWelcomeComponent }]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
